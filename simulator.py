372056fb3d199ab683eaf1fbef1e6ec5a78f97b9
omri <omrieitan@gmail.com>
2018-01-29 13:51:56 +0200

    sim update

--- simulator.py
+++ simulator.py
@@ -3,32 +3,29 @@ import sqlite3
 
 class _Simulator:
     def __init__(self):
-        if os.path.isfile("world.db"):
-            self.conn=sqlite3.connect("world.db")
-            self.occupiedTasks = []
+         self.conn=sqlite3.connect("world.db")
+         self.occupiedTasks = []
+         self.cursor = self.conn().cursor()
 
     def isFileExist(self):
         return os.path.isfile("world.db")
 
     def getAllTasks(self):
-        cursor = self.conn().cursor()
-        return cursor.execute("""
+        return self.cursor.execute("""
         SELECT id, task_name, worker_id, time_to_make FROM tasks
         """).fetchall()
 
-    def isoccupied(self,taskname):
+    def isTaskOccupied(self, taskname):
         return taskname in self.occupiedTasks
 
     def isWorkerBusy(self, workerId):
-        cursor = self.conn().cursor()
-        status=cursor.execute("""
+        status=self.cursor.execute("""
             SELECT status FROM workers WHERE id=(?)
             """, [workerId]).fetchone()
         return status == "idle"
 
     def getAllFinishedTasks(self):
-        cursor = self.conn().cursor()
-        return cursor.execute("""
+        return self.cursor.execute("""
                 SELECT task_name, worker_id, time_to_make FROM tasks WHERE time_to_make=0
                 """).fetchall()
 
@@ -39,15 +36,34 @@ class _Simulator:
         self.conn.execute("DELETE FROM tasks WHERE id=(?)",[taskId])
 
 # assume there is more then one step left for task, assume worker in working on task
-    def reduce(self,taskid, workerId):
+    def reduce(self,taskid):
+
 # assume worker is idle
-    def assign(self, taskName):
-        self.occupiedTasks.append(self,)
+    def assign(self, taskid):
+        self.cursor.execute("""
+                SELECT worker_id FROM tasks WHERE id=(?)
+                """, (taskid))
+        workerId = self.cursor.fetch()
+        self.occupiedTasks.append(self,workerId)
+
 # assume 0 steps left for task
-    def finishTask(self, taskName):
+    def finishTask(self, taskid):
+        self.cursor.execute("""
+                        SELECT worker_id FROM tasks WHERE id=(?)
+                        """, (taskid))
+        workerId = self.cursor.fetch()
+        self.updateWorkerStatus(self,workerId,"idle")
+        self.deleteTask(self,taskid)
+
+        self.cursor.execute("""
+                                SELECT name FROM workers WHERE id=(?)
+                                """, (workerId))
+        workerName = self.cursor.fetch()
+        print(workerName+" says: All Done!")
 
 
-    def isTasksNotEmpty(self, taskName):
+    def isTasksNotEmpty(self):
+        return len(self.occupiedTasks)!=0
 
 
 
